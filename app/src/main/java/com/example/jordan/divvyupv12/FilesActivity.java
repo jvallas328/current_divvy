5dbed8a5e223de24ab406a0f33a0154a398452ae
Garrett Boucher <gboucher@samford.edu>
2015-11-08 15:39:26 -0600

    Added the 'Files' page, linked it to the 'Your Files' button, and added the 'Create File' button (but it doesn't do anything yet)

--- app/src/main/java/com/example/jordan/divvyupv12/FilesActivity.java
+++ app/src/main/java/com/example/jordan/divvyupv12/FilesActivity.java
@@ -1,24 +1,22 @@
 package com.example.jordan.divvyupv12;
 
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
 
-public class HubActivity extends AppCompatActivity {
+public class FilesActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_hub);
+        setContentView(R.layout.activity_files);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_hub, menu);
+        getMenuInflater().inflate(R.menu.menu_files, menu);
         return true;
     }
 
@@ -35,11 +33,5 @@ public class HubActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
-    }
-
-    //Called when the user clicks the 'Your Files' button
-    public void goToFiles(View view){
-        Intent intent = new Intent(this, FilesActivity.class);
-        startActivity(intent);
     }
 }
