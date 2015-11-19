c288f43f40967457875fd96c12371dee4abf7aef
bteeling <bteeling@samford.edu>
2015-11-18 21:36:35 -0600

    Stuff we did Wednesday night.

--- app/src/main/java/com/example/jordan/divvyupv12/FilesActivity.java
+++ app/src/main/java/com/example/jordan/divvyupv12/FilesActivity.java
@@ -1,9 +1,6 @@
 package com.example.jordan.divvyupv12;
 
-import android.app.AlertDialog;
-import android.content.DialogInterface;
 import android.content.Intent;
-import android.inputmethodservice.ExtractEditText;
 import android.net.Uri;
 import android.os.StrictMode;
 import android.support.v7.app.AppCompatActivity;
@@ -13,29 +10,38 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
-import android.widget.ListAdapter;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.Toast;
-import android.widget.TextView;
 
 import org.json.JSONArray;
 import org.json.JSONObject;
 
 import java.io.BufferedInputStream;
+import java.io.BufferedReader;
 import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
-import java.util.ArrayList;
 import java.util.Scanner;
 
 public class FilesActivity extends AppCompatActivity {
+    public static Button save_file_button;
     String[] items;
+    String[] fileIDs;
+    String[] filecontentsarr;
+    String json = "";
+    URL url;
+    JSONArray arr = new JSONArray();
+    JSONArray arrUser = new JSONArray();
+    JSONArray arrShared = new JSONArray();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_files);
-        String json = "";
-        JSONArray arr = new JSONArray();
         int SDK_INT = android.os.Build.VERSION.SDK_INT;
         if (SDK_INT > 8)
         {//allow execution of network connection on the main thread
@@ -50,7 +56,7 @@ public class FilesActivity extends AppCompatActivity {
                 String query = builder.build().getEncodedQuery();
                 //System.out.println("The query: " + query);      //to verify query string
 
-                URL url = new URL("http://cslinux.samford.edu/codedb/getfilelist.php?" + query);
+                url = new URL("http://cslinux.samford.edu/codedb/getfilelist.php?" + query);
                 HttpURLConnection conn = (HttpURLConnection) url.openConnection();
                 System.out.println("The complete url: " + url); //to verify full url
                 try {//to get the response from server
@@ -60,31 +66,144 @@ public class FilesActivity extends AppCompatActivity {
                         json += httpin.nextLine();
                     }
                     arr = new JSONArray(json);
+                    arrUser = (JSONArray)arr.get(0);
+                    arrShared = (JSONArray)arr.get(1);
                     System.out.println("The JSON array contents: " + arr.toString());
-                    if(arr.length() == 0) { //if the json array is empty, then this user does not have any files
+                    if(arrUser.length() == 0 && arrShared.length() == 0) { //if the json array is empty, then this user does not have any files
                         Toast.makeText(FilesActivity.this, "You do not have any files yet.", Toast.LENGTH_LONG).show();
                     } else {                //user exists
                         JSONObject obj;
-                        String[] files = new String[arr.length()];
-                        final String[] filecontentsarr = new String[arr.length()];
-                        for (int i = 0; i < arr.length(); i++) {
-                            obj = arr.getJSONObject(i);
-                            System.out.println("The filename is: " + obj.getString("filename"));
-                            files[i] = obj.getString("filename");
-                            System.out.println("The filecontents is: " + obj.getString("textcontent"));
-                            filecontentsarr[i] = obj.getString("textcontent");
+                        String[] files = new String[arrUser.length() + arrShared.length()];
+                        fileIDs= new String[arrUser.length() + arrShared.length()];
+                        filecontentsarr= new String[arrUser.length() + arrShared.length()];
+                        int count = 0;
+                        for (int i = 0; i < arrUser.length(); i++) {
+                            obj = arrUser.getJSONObject(i);
+                            System.out.println("(User) The filename is: " + obj.getString("filename"));
+                            files[count] = obj.getString("filename");
+                            System.out.println("(User) The file ID is: " + obj.getString("id"));
+                            fileIDs[count] = obj.getString("id");
+                            count++;
+                        }
+                        for (int i = 0; i < arrShared.length(); i++) {
+                            obj = arrShared.getJSONObject(i);
+                            System.out.println("(Shared) The filename is: " + obj.getString("filename"));
+                            files[count] = obj.getString("filename");
+                            System.out.println("(Shared) The file ID is: " + obj.getString("codefile_id"));
+                            fileIDs[count] = obj.getString("codefile_id");
+                            count++;
                         }
                         ArrayAdapter<String> filesAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,files);
                         ListView listView = (ListView) findViewById(R.id.listView);
                         listView.setAdapter(filesAdapter);
                         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
                             @Override
-                            public void onItemClick(AdapterView<?> adapter, View view, int position, long id) {
-                                System.out.println(id);
-                                System.out.println(filecontentsarr[(int)id]);
-                                String string = filecontentsarr[(int)id];
-                                TextView text = (TextView)findViewById(R.id.fileContentsBox);
-                                text.setText(string);
+                            public void onItemClick(AdapterView<?> adapter, View view, int position, final long id) {
+                                try { //must surround with try/catch to filter errors
+                                    String contents = "";
+                                    Uri.Builder builder = new Uri.Builder()
+                                            .appendQueryParameter("fileid", fileIDs[(int)id])
+                                            .appendQueryParameter("db", "codedb");
+                                    String query = builder.build().getEncodedQuery();
+                                    System.out.println("The query: " + query);      //to verify query string
+
+                                    url= new URL("http://cslinux.samford.edu/codedb/loadfile.php?" + query);
+                                    HttpURLConnection conn2 = (HttpURLConnection) url.openConnection();//MAKE GLOBAL LATER
+                                    System.out.println("The complete url: " + url); //to verify full url
+                                    try {//to get the response from server
+                                        InputStream in = new BufferedInputStream(conn2.getInputStream());
+                                        Scanner httpin = new Scanner(in);
+                                        for(int i = 0; httpin.hasNextLine(); i++) {
+                                            if(i != 0) {
+                                                contents += "\n" + httpin.nextLine();
+                                            }else {
+                                                contents += httpin.nextLine();
+                                            }
+                                        }
+                                        final String finalContents = contents;
+                                        //arr = new JSONArray(json);
+                                        System.out.println("The contents: " + contents);
+                                        if(contents.equals("")) { //if the json array is empty, then this user does not have any files
+                                            Toast.makeText(FilesActivity.this, "You do not have any files yet.", Toast.LENGTH_LONG).show();
+                                        } else {                //user exists
+                                            EditText text = (EditText)findViewById(R.id.fileContentsBox);;
+                                            text.setText(contents);
+
+                                            save_file_button = (Button)findViewById(R.id.save_file);
+                                            save_file_button.setOnClickListener(
+                                                    new View.OnClickListener() {
+                                                        @Override
+                                                        public void onClick(View v) {
+                                                            EditText newtext = (EditText) findViewById(R.id.fileContentsBox);
+                                                            try { //must surround with try/catch to filter errors
+                                                                //String contents = "";
+                                                                Uri.Builder builder = new Uri.Builder()
+                                                                        .appendQueryParameter("origtext", finalContents)
+                                                                        .appendQueryParameter("newtext", newtext.getText().toString())
+                                                                        .appendQueryParameter("userid", Globals.getInstance().userID)
+                                                                        .appendQueryParameter("fileid", fileIDs[(int)id])
+                                                                        .appendQueryParameter("db", "codedb");
+                                                                String query = builder.build().getEncodedQuery();
+                                                                System.out.println("The query: " + query);      //to verify query string
+
+                                                                url= new URL("http://cslinux.samford.edu/codedb/patchmake.php?" + query);
+                                                                HttpURLConnection conn3 = (HttpURLConnection) url.openConnection();//MAKE GLOBAL LATER
+                                                                conn3.setRequestMethod("POST");
+                                                                conn3.setDoOutput(true);
+                                                                conn3.setDoInput(true);
+                                                                System.out.println("The complete POST url: " + url); //to verify full url
+                                                                try {//to get the response from server
+                                                                    String postContents = "";
+                                                                    BufferedReader rd = new BufferedReader(new InputStreamReader(conn3.getInputStream()));
+                                                                    StringBuilder sb = new StringBuilder();
+                                                                    String line;
+                                                                    while((line = rd.readLine()) != null) {
+                                                                        postContents += line;
+                                                                        System.out.println("entered for loop");
+                                                                    }
+                                                                    System.out.println("line " + line);
+                                                                    //arr = new JSONArray(json);
+                                                                    System.out.println("The POST contents: " + postContents);
+                                                                    /*if(postContents.equals("")) { //if the json array is empty, then this user does not have any files
+                                                                        Toast.makeText(FilesActivity.this, "You do not have any files yet.", Toast.LENGTH_LONG).show();
+                                                                    } else {                //user exists
+                                                                        //EditText text = (EditText)findViewById(R.id.fileContentsBox);;
+                                                                        //text.setText(postontents);
+
+                                                                        save_file_button = (Button)findViewById(R.id.save_file);
+                                                                        save_file_button.setOnClickListener(
+                                                                                new View.OnClickListener() {
+                                                                                    @Override
+                                                                                    public void onClick(View v) {
+
+                                                                                    }
+                                                                                }
+                                                                        );
+                                                                    }//password for db is
+                                                                */} catch (Exception e) {
+                                                                    e.printStackTrace();
+                                                                }
+                                                                finally{//disconnect after making the connection and executing the query
+                                                                    conn3.disconnect();
+                                                                }
+                                                            }catch (Exception e){
+                                                                e.printStackTrace();
+                                                            }
+                                                        }
+                                                    }
+                                            );
+                                        }//password for db is
+                                    } catch (Exception e) {
+                                        e.printStackTrace();
+                                    }
+                                    finally{//disconnect after making the connection and executing the query
+                                        conn2.disconnect();
+                                    }
+                                }catch (Exception e){
+                                    e.printStackTrace();
+                                }
+
+
                             }
                         });
 
